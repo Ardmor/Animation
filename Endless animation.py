ee23c56a01e60d93ce78955fa0453dc74bf7a67f
Ardmor <110854529+Ardmor@users.noreply.github.com>
2025-05-07 14:48:23 +0300

    Add files via upload

---
+++ Endless animation.py
@@ -0,0 +1,170 @@
+i = 0
+total = 0
+while i < 10:
+    total += i
+    print('\n' * 4, '*')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('\n' * 2, '\n', '*', '\n', '*' * 2, '\n', '*', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('\n' * 2, '*', '\n', '*' * 2, '\n', '*' * 3, '\n', '*' * 2,  '\n', '*', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('\n', '*', '\n', '*' * 2, '\n', '*' * 3, '\n', '*' * 4, '\n', '*' * 3, '\n', '*' * 2,  '\n', '*', '\n', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print(' *', '\n', '*' * 2, '\n', '*' * 3, '\n', '*' * 4, '\n', '*' * 5, '\n', '*' * 4, '\n', '*' * 3, '\n', '*' * 2, '\n', '*')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print(' *', '\n', '*' * 3, '\n', '*' * 4, '\n', '*' * 5, '\n', '*' * 6 ,'\n', '*' * 5, '\n', '*' * 4, '\n', '*' * 3, '\n', ' *', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('  *', '\n', ' *','*' * 2, '\n', '*' * 5, '\n', '*' * 6, '\n', '*' * 7 ,'\n', '*' * 6, '\n', '*' * 5, '\n', ' *','*' * 2, '\n', '  *', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('   *', '\n', '  *','*' * 2, '\n', ' *', '*' * 4, '\n', '*' * 7, '\n', '*' * 8 ,'\n', '*' * 7, '\n', ' *', '*' * 4, '\n', '  *','*' * 2, '\n', '   *', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('    *', '\n', '   *','*' * 2, '\n', '  *', '*' * 4, '\n', ' *', '*' * 6, '\n', '*' * 9 ,'\n', ' *', '*' * 6, '\n', '  *', '*' * 4, '\n', '   *','*' * 2, '\n', '    *', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('     *', '\n', '    *','*' * 2, '\n', '   *', '*' * 4, '\n', '  *', '*' * 6, '\n', ' *', '*' * 8 ,'\n', '  *', '*' * 6, '\n', '   *', '*' * 4, '\n', '    *','*' * 2, '\n', '     *', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    # Движение
+    print('      *', '\n', '     *','*' * 2, '\n', '    *', '*' * 4, '\n', '   *', '*' * 6, '\n', '  *', '*' * 8 ,'\n', '   *', '*' * 6, '\n', '    *', '*' * 4, '\n', '     *','*' * 2, '\n', '      *', sep = '') #скопировал
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('       *', '\n', '      *','*' * 2, '\n', '     *', '*' * 4, '\n', '    *', '*' * 6, '\n', '   *', '*' * 8 ,'\n', '    *', '*' * 6, '\n', '     *', '*' * 4, '\n', '      *','*' * 2, '\n', '       *', sep = '') #скопировал
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('        *', '\n', '       *','*' * 2, '\n', '      *', '*' * 4, '\n', '     *', '*' * 6, '\n', '    *', '*' * 8 ,'\n', '     *', '*' * 6, '\n', '      *', '*' * 4, '\n', '       *','*' * 2, '\n', '        *', sep = '') #скопировал
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    #пошло копирование
+    print('         *', '\n', '        *','*' * 2, '\n', '       *', '*' * 4, '\n', '      *', '*' * 6, '\n', '     *', '*' * 8 ,'\n', '      *', '*' * 6, '\n', '       *', '*' * 4, '\n', '        *','*' * 2, '\n', '         *', sep = '') #скопировал
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('          *', '\n', '         *','*' * 2, '\n', '        *', '*' * 4, '\n', '       *', '*' * 6, '\n', '      *', '*' * 8 ,'\n', '       *', '*' * 6, '\n', '        *', '*' * 4, '\n', '         *','*' * 2, '\n', '          *', sep = '') #скопировал
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('           *', '\n', '          *','*' * 2, '\n', '         *', '*' * 4, '\n', '        *', '*' * 6, '\n', '       *', '*' * 8 ,'\n', '        *', '*' * 6, '\n', '         *', '*' * 4, '\n', '          *','*' * 2, '\n', '           *', sep = '') #скорировал
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('            *', '\n', '           *','*' * 2, '\n', '          *', '*' * 4, '\n', '         *', '*' * 6, '\n', '        *', '*' * 8 ,'\n', '         *', '*' * 6, '\n', '          *', '*' * 4, '\n', '           *','*' * 2, '\n', '            *', sep = '') #скопировал
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    
+    #возврат
+    print('           *', '\n', '          *','*' * 2, '\n', '         *', '*' * 4, '\n', '        *', '*' * 6, '\n', '       *', '*' * 8 ,'\n', '        *', '*' * 6, '\n', '         *', '*' * 4, '\n', '          *','*' * 2, '\n', '           *', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('          *', '\n', '         *','*' * 2, '\n', '        *', '*' * 4, '\n', '       *', '*' * 6, '\n', '      *', '*' * 8 ,'\n', '       *', '*' * 6, '\n', '        *', '*' * 4, '\n', '         *','*' * 2, '\n', '          *', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('         *', '\n', '        *','*' * 2, '\n', '       *', '*' * 4, '\n', '      *', '*' * 6, '\n', '     *', '*' * 8 ,'\n', '      *', '*' * 6, '\n', '       *', '*' * 4, '\n', '        *','*' * 2, '\n', '         *', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('        *', '\n', '       *','*' * 2, '\n', '      *', '*' * 4, '\n', '     *', '*' * 6, '\n', '    *', '*' * 8 ,'\n', '     *', '*' * 6, '\n', '      *', '*' * 4, '\n', '       *','*' * 2, '\n', '        *', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('       *', '\n', '      *','*' * 2, '\n', '     *', '*' * 4, '\n', '    *', '*' * 6, '\n', '   *', '*' * 8 ,'\n', '    *', '*' * 6, '\n', '     *', '*' * 4, '\n', '      *','*' * 2, '\n', '       *', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('      *', '\n', '     *','*' * 2, '\n', '    *', '*' * 4, '\n', '   *', '*' * 6, '\n', '  *', '*' * 8 ,'\n', '   *', '*' * 6, '\n', '    *', '*' * 4, '\n', '     *','*' * 2, '\n', '      *', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('     *', '\n', '    *','*' * 2, '\n', '   *', '*' * 4, '\n', '  *', '*' * 6, '\n', ' *', '*' * 8 ,'\n', '  *', '*' * 6, '\n', '   *', '*' * 4, '\n', '    *','*' * 2, '\n', '     *', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    
+    #исчезновение
+    print('    *', '\n', '   *','*' * 2, '\n', '  *', '*' * 4, '\n', ' *', '*' * 6, '\n', '*' * 9 ,'\n', ' *', '*' * 6, '\n', '  *', '*' * 4, '\n', '   *','*' * 2, '\n', '    *', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('   *', '\n', '  *','*' * 2, '\n', ' *', '*' * 4, '\n', '*' * 7, '\n', '*' * 8 ,'\n', '*' * 7, '\n', ' *', '*' * 4, '\n', '  *','*' * 2, '\n', '   *', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('  *', '\n', ' *','*' * 2, '\n', '*' * 5, '\n', '*' * 6, '\n', '*' * 7 ,'\n', '*' * 6, '\n', '*' * 5, '\n', ' *','*' * 2, '\n', '  *', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print(' *', '\n', '*' * 3, '\n', '*' * 4, '\n', '*' * 5, '\n', '*' * 6 ,'\n', '*' * 5, '\n', '*' * 4, '\n', '*' * 3, '\n', ' *', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print(' *', '\n', '*' * 2, '\n', '*' * 3, '\n', '*' * 4, '\n', '*' * 5, '\n', '*' * 4, '\n', '*' * 3, '\n', '*' * 2, '\n', '*')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('\n', '*', '\n', '*' * 2, '\n', '*' * 3, '\n', '*' * 4, '\n', '*' * 3, '\n', '*' * 2,  '\n', '*', '\n', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('\n' * 2, '*', '\n', '*' * 2, '\n', '*' * 3, '\n', '*' * 2,  '\n', '*', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
+    print('\n' * 2, '\n', '*', '\n', '*' * 2, '\n', '*', sep = '')
+    import time
+    time.sleep(0.01)
+    import os
+    os.system('clear')
